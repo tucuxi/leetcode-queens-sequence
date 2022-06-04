52a258a6f73735b24c51ddfd72a0251e85145226
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2022-06-04 17:09:46 +0200

    Add solution using sequence

---
+++ src/main/kotlin/SolveNQueens.kt
@@ -0,0 +1,40 @@
+import kotlin.math.abs
+
+fun solveNQueens(n: Int) = sequence {
+    val queens = IntArray(n)
+
+    fun safe(row: Int, col: Int): Boolean =
+        (0 until row).none { i -> abs(col - queens[i]).let { it == 0 || it == row - i } }
+
+    suspend fun SequenceScope<List<String>>.rec(k: Int) {
+        if (k == n) {
+            yield(queens
+                .map { i ->
+                    (0 until n)
+                        .map { j -> if (j == i) 'Q' else '.' }
+                        .joinToString("")
+                }
+            )
+        } else {
+            (0 until n)
+                .filter { safe(k, it) }
+                .forEach {
+                    queens[k] = it
+                    rec(k + 1)
+                }
+        }
+    }
+
+    rec(0)
+}
+
+fun main() {
+    solveNQueens(8)
+        .take(3)
+        .forEach { solution ->
+            solution.forEach { row ->
+                println(row)
+            }
+            println()
+        }
+}
