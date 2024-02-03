c105c782f36d3db8015b3ff5daa0114d51057f87
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2024-02-03 11:23:56 +0100

    Upgrade Gradle Wrapper to v8.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     kotlin("jvm") version "1.9.22"
-    id("io.gitlab.arturbosch.detekt").version("1.23.3")
+    id("io.gitlab.arturbosch.detekt").version("1.23.4")
 }
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
+testing {
+    suites {
+        val test by getting(JvmTestSuite::class) {
+            useJUnitJupiter()
+        }
+    }
 }
